1d9fb6dfb8aa3e6286a279f29dc5bdea6e382839
Sai Ganesh <44020019+luckyganesh@users.noreply.github.com>
2019-01-11 17:39:35 +0530

    added some more functionality to install.sh

--- install.sh
+++ install.sh
@@ -88,3 +88,15 @@ sudo npm install -g nyc
 sudo npm install -g readline-sync
 sudo npm install -g mocha
 sudo npm install -g prettier
+sudo npm install -g chalk
+
+echo "Installing zsh-autoSuggestions";
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions;
+git config --global user.name "Sai Ganesh";
+git config --global user.email "44020019+luckyganesh@users.noreply.github.com";
+mkdir -p .vim/pack/plugins/start;
+git clone https://github.com/sickill/vim-monokai.git
+git clone https://github.com/scrooloose/nerdtree.git
+git clone https://github.com/airblade/vim-gitgutter.git
+git clone https://github.com/vim-airline/vim-airline.git;
+mv vim-airline vim-gitgutter nerdtree .vim/pack/plugins/start
