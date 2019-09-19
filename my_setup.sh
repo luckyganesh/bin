6e8a5d45a52e00f3030fcbfffbf4a35d84a135ad
Sai Ganesh <44020019+luckyganesh@users.noreply.github.com>
2019-09-19 10:14:30 +0530

    Added cloning of mac-setup in setup

--- my_setup.sh
+++ my_setup.sh
@@ -2,6 +2,7 @@
 
 git clone https://github.com/luckyganesh/bin.git ~/bin
 git clone https://github.com/luckyganesh/dotfiles.git ~/dotfiles
+git clone https://github.com/luckyganesh/mac-setup.git
 
 rm ~/.gitconfig ~/.zshrc ~/.vimrc
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
